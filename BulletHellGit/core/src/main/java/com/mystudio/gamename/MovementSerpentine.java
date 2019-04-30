38cbbb265f48927d9fb4cbc027c361633fd5711f
Noah <46874812+SaviorKiwi@users.noreply.github.com>
2019-04-30 13:41:51 -0500

    Combined changes

--- BulletHellGit/core/src/main/java/com/mystudio/gamename/MovementSerpentine.java
+++ BulletHellGit/core/src/main/java/com/mystudio/gamename/MovementSerpentine.java
@@ -1,4 +1,5 @@
 package com.mystudio.gamename;
+import java.util.Random;
 /*
     This class enables serpentine movement. This can be used for moving back an forth on an angle. Moving back and worth while moving down, or moving up and down while going right
     or any other similar combination;
@@ -14,6 +15,8 @@ public class MovementSerpentine extends MovementBase {
     private int maxStepsUpDown;
     private int currentStepsUpDown;
 
+       //How likely the ship is to shoot at any given time
+    private int shotChance;
     /*
         @param String texturepath, the string which leads you to the sprite,
         @float x, x coordinate
@@ -23,11 +26,12 @@ public class MovementSerpentine extends MovementBase {
         @int maxStepsToSide, the number of frames the enemy moves to the side before revering
         @int maxStepsUpDown, the number of frames the enemy moves up or down before reversing.
      */
-    public MovementSerpentine(String texturePath, float x, float y, int xSpeed, int ySpeed, int maxStepsToSide, int maxStepsUpDown) {
+     public MovementSerpentine(String texturePath, float x, float y, int xSpeed, int ySpeed, int maxStepsToSide, int maxStepsUpDown, int shotChance) {
         super(texturePath,x,y,xSpeed,ySpeed);
 
         this.maxStepsToSide = maxStepsToSide;
         this.maxStepsUpDown = maxStepsUpDown;
+        this.shotChance = shotChance;
     }
 
     public String update(String currStat){
@@ -53,6 +57,14 @@ public class MovementSerpentine extends MovementBase {
             currentStepsUpDown = 0;
         } else if (maxStepsUpDown != -1) {
             currentStepsUpDown++;
+        }
+        //BULLET SPAWNER
+        Random roller = new Random();
+        int temp = roller.nextInt(100);
+
+       //uses shotChance variable to determine how often the ship will shoot. Rolled once every update. 
+        if (temp<=shotChance){
+        MyMini2DxGame.enemies.add(new EnemyBullet(super.GetPoint().getX(),super.GetPoint().getY()));
         }
 
 
