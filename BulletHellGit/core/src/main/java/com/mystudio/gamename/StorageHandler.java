5f7f7843a387db1091f9ca6b42f901d650a9504e
John <John@DESKTOP-QC2R0O9>
2019-04-30 22:26:46 -0500

    High score storage and retrieval working version

--- BulletHellGit/core/src/main/java/com/mystudio/gamename/StorageHandler.java
+++ BulletHellGit/core/src/main/java/com/mystudio/gamename/StorageHandler.java
@@ -5,7 +5,12 @@
  */
 package com.mystudio.gamename;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.GdxRuntimeException;
+import static com.mystudio.gamename.StorageHandler.AppendScore;
 import java.util.ArrayList;
+import java.util.Stack;
 import org.mini2Dx.core.Mdx;
 import org.mini2Dx.core.playerdata.PlayerDataException;
 
@@ -17,29 +22,70 @@ public class StorageHandler {
     
     //this is the object that actually gets written to memory
     private static PlayerData playerData = new PlayerData();
+    private static final FileHandle FILE = Gdx.files.local("savedata.txt");
     
+    public static void SetPlayerData(PlayerData pd){
+        playerData = pd;
+    }
+    
+    public static PlayerData GetPlayerData(){
+        return playerData;
+    }
     /*
         Attempts to load data from the file system of the computer.
-        If this failes, then an error is printed
+        If this failes, then false is returned
     */
-    public static void Load(){
+    public static boolean Load(){
         try {
-            Mdx.playerData.writeJson(playerData, "playerdata.json");
-        } catch (PlayerDataException e){
-            System.out.println("No Player Data Avaiable To Load");
+            String temp = FILE.readString();
+            String[] rows = temp.split("\n");
+            for(String str: rows){
+                String[] columns = str.split(",");
+                int score = 0;
+                try {
+                    score = Integer.parseInt(columns[1]);
+                } catch (NumberFormatException e) {
+                    score = -1;
+                }
+                
+                AppendScore(columns[0],score);
+            }
+            
+            return true;
+        } catch (GdxRuntimeException e) {
+            return false;
         }
     }
     
     /*
-        Attempts to write data to the file system of the computer
-        If this fails, an error message is printed
+        Stores data as a string
+        Seperated by columns and linebreaks
+        May the gods have mercy on my soul
+        
     */
-    public static void Store() {
+    public static boolean Store() {
+        /*
         try {
             Mdx.playerData.writeJson(playerData, "playerdata.json");
+            return true;
         } catch(PlayerDataException e){
             System.out.println("Could Not Save Data");
+            return false;
+        }*/
+        
+        PlayerScore[] temp = playerData.RetrieveScores();
+        
+        //resets the file contents
+        FILE.writeString("", false);
+        
+        
+        for(PlayerScore score: temp) {
+            FILE.writeString(score.GetPlayerName() + "," + score.GetPlayerScore()+"\n", true);
         }
+        
+      //System.out.println(FILE.readString());
+        
+        return true;
     }
     
     /*
@@ -63,7 +109,7 @@ public class StorageHandler {
     
         Returns a sorted array of scores
     */
-    public static PlayerScore[] GiveScoresSortedByHighest() {
+    public static PlayerScore[] GiveScoresSortedByLowest() {
         PlayerScore[] temp = StorageHandler.GiveScoresUnsorted();
         
         //Bubble sorting, for fun and profit
@@ -83,8 +129,8 @@ public class StorageHandler {
     /*
         Same deal, sorted by lowest
     */
-    public static PlayerScore[] GiveScoresSortedByLowest(){
-        PlayerScore[] temp = StorageHandler.GiveScoresSortedByHighest();
+    public static PlayerScore[] GiveScoresSortedByHighest(){
+        PlayerScore[] temp = StorageHandler.GiveScoresSortedByLowest();
         
         PlayerScore[] reverse = new PlayerScore[temp.length];
         
